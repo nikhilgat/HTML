d1b230ca511e1e251bc1e0945a3775144effa431
nikhil gatla <62739007+nikhilgat@users.noreply.github.com>
2022-07-20 19:05:03 +0530

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//for login
+
+function validate()
+{
+    var username = document.getElementById("MailId");
+    var password = document.getElementById("Password");
+
+    if(username.value() == "" || password.value() =="")
+    {
+        alert("Please provide the input");
+    }
+    else 
+    {
+
+    }
+
+}
